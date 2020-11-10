60e81942cff3b7fb7f0c6f931e839d3078f2cdbc
YevheniiLutai <53271797+YevheniiLutai@users.noreply.github.com>
2020-11-11 01:52:43 +0200

    first commit

---
+++ Homework_1/script.js
@@ -0,0 +1,102 @@
+    
+    //--------------------------------------------------- Task_2
+    //Task_2 ---------------------------------------------------
+
+    console.log("Task 2: Lutai");
+
+    //----------------------------------------------------------
+
+
+    //Task_3 ---------------------------------------------------
+
+    let a = 5;
+    let b = 10;
+    console.log("Task 3:", a, b);
+    a = b;
+    console.log("Task 3:", a);
+   
+    //----------------------------------------------------------
+
+
+    //Task_4 ---------------------------------------------------
+    let x;
+    let first_object = {
+        String: "Yevhenii", 
+        Number: 22, 
+        Boolean: true,
+        Undefined: x, 
+        Null: null
+    };
+    
+    console.log("Task 4.1:", first_object);
+
+    let second_object = {
+        String: "Lutai", 
+        Number: 95, 
+        Boolean: false,
+        Undefined: undefined, 
+        Null: null
+    };
+    
+    console.log("Task 4.2:", second_object);
+
+    //----------------------------------------------------------
+
+
+    //Task_5 ---------------------------------------------------
+
+    let isAdmin = confirm("Are 18+ years old?");
+
+    console.log("Task 5:", isAdmin);
+
+    //----------------------------------------------------------
+
+
+    //Task_6 ---------------------------------------------------
+
+    let name = "Yevhenii";
+    let surname = "Lutai";
+    let group = 548;
+    let Date_of_Birthday = "04.03.1998";
+    let married = false;
+
+    console.log("Task 6: ", typeof group, typeof married, typeof name, typeof surname, typeof Date_of_Birthday);
+
+    let m;
+    let n = null;
+
+    console.log("Type 6: ", typeof n, typeof m);
+    //----------------------------------------------------------
+
+
+    //Task_7 ---------------------------------------------------
+
+    let login = prompt("What is your login?", "Yevhenii");
+    let mail = prompt("Enter your mail:", "jenia940@gmail.com");
+    let password = prompt("Сreate your password:", "qwe123456789qwe");
+
+    alert("Dear " + login + ", your email is " + mail + ", your password is " + password);
+
+    //----------------------------------------------------------
+
+
+    //Task_8 ---------------------------------------------------
+
+    let second = 60;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+    let month_28 = day * 28;
+    let month_29 = day * 29;
+    let month_30 = day * 30;
+    let month_31 = day * 31;
+
+    //Spaces for a better view in alert window
+    alert("Sencond in hour: " + hour + ";                                                                        " +
+    "Second in day: " + day + ";                                                                      " +
+    "Second in month(28 days): " + month_28 + ";                                            " +
+    "Second in month(29 days): " + month_29 + ";                                            " +
+    "Second in month(30 days): " + month_30 + ";                                            " +
+    "Second in month(31 days): " + month_31 + ";                                            ");
+
+    //----------------------------------------------------------
