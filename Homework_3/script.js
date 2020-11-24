fa320f692c6541be835bbdd445f3db6a73ff9e07
YevheniiLutai <53271797+YevheniiLutai@users.noreply.github.com>
2020-11-24 02:41:26 +0200

    Finish homework 3(need to chech all task) and add little update to other file

--- Homework_3/script.js
+++ Homework_3/script.js
@@ -3,6 +3,8 @@
 
 let number = [7, 1, 14, 19, 27]
 let sum = 0;
+
+
 for(let i = 0; i < number.length; i++ ) {
     sum += number[i];
 }
@@ -24,6 +26,7 @@ console.log("Task 1.2: Sum = " + sum1);
 //Task_2 ---------------------------------------------------
 
 for(let i = 0; i <= 15; i++ ) {
+
     if(i % 2 == 0) {
         console.log("Task 2:", i + " even");
     }
@@ -39,8 +42,12 @@ for(let i = 0; i <= 15; i++ ) {
 
 function randArray(k) {
     let Array = [];
+    min = Math.ceil(1);
+    max = Math.floor(500);
+
+
     for (let i = 0; i < k; i++) {
-        Array.push(Math.round(Math.random() * 500));
+        Array.push(Math.floor(Math.random() * (max - min + 1)) + min);
     } 
     console.log("Task 3:", Array);
 };
@@ -54,8 +61,10 @@ randArray(15);
 
 let a = Number(prompt("Enter a", "5"));
 let b = Number(prompt("Enter a", "3"));
+
 function raiseToDegree(a, b) {
     let powAB = Math.pow(a, b);
+
     console.log("Task 4: ", powAB); 
 };
 
@@ -68,8 +77,11 @@ raiseToDegree(a, b);
 
 function findMin() {
     let arr = Array.from(arguments);
+
     console.log("Task 5:", arr);
+
     arr.sort();
+
     console.log("Task 5, min number =", arr[0]);
 };
 
@@ -82,22 +94,25 @@ findMin(7,4,-5,5,8);
 //Task_6 ---------------------------------------------------
 
 function findUnique(arr) {
+
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] === arr[j]) {
-                let result = false;
-                console.log("Task 6:", result);
-                return;
+                /*let result = false;
+                console.log("Task 6:", result);*/
+
+                return false;
             };
         };
     };
-    result = true;
-    console.log("Task 6:", result);
-    return;
+    /*result = true;
+    console.log("Task 6:", result);*/
+
+    return true;
 };
 
-findUnique([1, 2, 3, 5, 3]);  
-findUnique([1, 2, 3, 5, 11]); 
+console.log("Task 6:", findUnique([1, 2, 3, 5, 3]));  
+console.log("Task 6:", findUnique([1, 2, 3, 5, 11])); 
 
 //----------------------------------------------------------
 
@@ -106,6 +121,8 @@ findUnique([1, 2, 3, 5, 11]);
 
 function lastElement(arr, x) {
     let i = 0;
+
+
     if (x == null) {
         return arr.pop();
     } else if (x >= arr.length) {
@@ -123,26 +140,26 @@ function lastElement(arr, x) {
 console.log("Task 7:", lastElement([3, 4, 10, -5]));      // -5
 console.log("Task 7:", lastElement([3, 4, 10, -5],2));   // [10, -5]
 console.log("Task 7:", lastElement([3, 4, 10, -5],8));   // [3, 4, 10, -5]
-console.log("Task 7:", lastElement([3, 4, 10, -5, 8,-7, 45, 55, 65],5));
+
 //----------------------------------------------------------
 
 
 
 //Task_8 ---------------------------------------------------
-
 function changeString(text) {
-    textArr = text.split();
-    console.log(textArr);
+    
+    console.log("Task 8, before:", text);
+    textArr = text.split('');
+    textArr[0] = textArr[0].toUpperCase();
+
     for (let i = 0; i < textArr.length; i++) {
-        if(textArr[i] = 'a') {
-            //let j = i + 1;
-            console.log(textArr[i].toUpperCase());
-        }
-        console.log(textArr);
-    }
-    console.log(textArr);
-}
+        if(textArr[i] == ' ') {
+            textArr[i + 1] = textArr[i + 1].toUpperCase();
+        };
+    };
+    console.log("Task 8, after:", textArr.join(''));
+};
 
 changeString('i love java script');
-
+changeString('Hello my name yevhenii Lutai and I love java script');
 //----------------------------------------------------------
