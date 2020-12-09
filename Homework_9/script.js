7db202b06bcbb01ebe5fc2ab234b2474aa653fd9
YevheniiLutai <53271797+YevheniiLutai@users.noreply.github.com>
2020-12-09 19:52:40 +0200

    Finish Homework 9, but need check

--- Homework_9/script.js
+++ Homework_9/script.js
@@ -9,21 +9,20 @@ $('.inner').css('font-size', '35px')
 //-----------------------------------------------------------\\
 
 //************************** Task_3 *************************\\
+
 $('div').each(() => {
-  $('div').insertBefore($(this).prev());
+    $('div').insertBefore($(this).prev());
 });
+
 //-----------------------------------------------------------\\
 
 //************************** Task_4 *************************\\
 
-let checked = $('input:checked').val();
-console.log($('rezult').html())
-if ($('input:checked').length == 3) {
-   $('input').attr('disable', 'true')
-} ;
-//let  tagInputCheked = document.querySelectorAll('input:checked')
-  //  if (tagInputCheked.length == 3) {
-    //    document.querySelectorAll('input').forEach(elem => elem.disabled=true);
-    //}
+function checkBoxDisable() {
+  if ($('input:checked').length == 3) {
+      $('input').attr('disabled', 'true');
+  };
+};
 
+$('input').change(checkBoxDisable);
 //-----------------------------------------------------------\\
