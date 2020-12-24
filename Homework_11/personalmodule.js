8a65bc3ba3f1c43d764b61f8552d11a2adb5ce44
YevheniiLutai <53271797+YevheniiLutai@users.noreply.github.com>
2020-12-24 13:06:29 +0200

    Finish homework 11, but need check

---
+++ Homework_11/personalmodule.js
@@ -0,0 +1,20 @@
+
+const currentDate = () => {
+    return new Date();
+};
+const Date = () => {
+    let day = new Date();
+    let currentHour = day.getHours();
+    if (currentHour >= 23 && currentHour < 5) {
+      return "Good night, ";
+    } else if (currentHour >= 5 && currentHour < 11) {
+      return "Good morning, ";
+    } else if (currentHour >= 11 && currentHour < 17) {
+      return "Good afternoon, ";
+    } else if (currentHour >= 17 && currentHour < 23) {
+      return "Good evening, ";
+    }
+};
+  
+exports.currentDate = currentDate;
+exports.Date = Date;
---
+++ Homework_11/script.js
@@ -0,0 +1,29 @@
+
+//************************** Task_1 *************************\\
+const path = require("path");
+const http = require("http");
+const os = require("os");
+
+http.createServer(function (request, response) {
+response.writeHead(200, {'Content-Type': 'text/html'});
+response.write("<h1>System information</h1><br>"
+`<p>Current user name: ${os.userInfo().username}<br>
+OS type: ${os.type()}<br>
+System work time: ${((os.uptime()) / 60).toFixed(2)} minutes<br>
+Current work directory:  ${__dirname}<br>
+Server file name: ${path.basename(__filename)}</p>`);
+esponse.end();
+}).listen(5000);
+//-----------------------------------------------------------\\
+
+//************************** Task_2 *************************\\
+    
+// const http = require("http");
+// const os = require("os");
+// const personalModule = require('./personalmodule');
+// http.createServer((request, response) => {
+// response.writeHead(200, {'Content-Type': 'text/plain'});
+// response.end(`${personalModule.greet(os.userInfo().username)}`);
+// }).listen(5000);
+
+//-----------------------------------------------------------\\
