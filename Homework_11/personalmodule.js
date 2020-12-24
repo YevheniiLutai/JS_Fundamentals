fed11c43a9351e06447d32d30f8f252985d5568c
YevheniiLutai <53271797+YevheniiLutai@users.noreply.github.com>
2020-12-24 13:32:10 +0200

    Finish homework 11

--- Homework_11/personalmodule.js
+++ Homework_11/personalmodule.js
@@ -1,4 +1,6 @@
+//****** Code for Task 2 ******\\
 
+//************************** Task_2 **************************\\
 const currentDate = () => {
     return new Date();
 };
@@ -8,13 +10,14 @@ const Date = () => {
     if (currentHour >= 23 && currentHour < 5) {
       return "Good night, ";
     } else if (currentHour >= 5 && currentHour < 11) {
-      return "Good morning, ";
+      return "Good morning ,";
     } else if (currentHour >= 11 && currentHour < 17) {
       return "Good afternoon, ";
     } else if (currentHour >= 17 && currentHour < 23) {
       return "Good evening, ";
     }
 };
-  
+
 exports.currentDate = currentDate;
 exports.Date = Date;
+//-----------------------------------------------------------\\
